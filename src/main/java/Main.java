286a8d290d457b8f946b5aa827411d6ced05c5fa
Bamse <bbandresen@hotmail.com>
2019-11-08 12:23:22 +0100

    maven version updatering

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import gui_main.GUI;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+    }
+}
